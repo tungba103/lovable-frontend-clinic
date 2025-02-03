afd623d2627fa9fdc94b42c606a9e6136e05e4ae
tungba103 <tungbt.work@protonmail.com>
2025-02-03 10:55:56 +0700

    fix 5

--- src/components/AsyncButton.tsx
+++ src/components/AsyncButton.tsx
@@ -1,5 +1,5 @@
 import { Loader2 } from 'lucide-react';
-import { Button } from './ui/button';
+import { Button, ButtonProps } from './ui/button';
 import { cn } from '@/lib/utils';
 
 const AsyncButton = ({
@@ -8,18 +8,21 @@ const AsyncButton = ({
   isLoading,
   className,
   disabled,
+  size,
 }: {
   children: React.ReactNode;
   onClick: () => void;
   isLoading: boolean;
   className?: string;
   disabled?: boolean;
+  size?: ButtonProps['size'];
 }) => {
   return (
     <Button
       onClick={onClick}
       disabled={isLoading || disabled}
       className={cn('min-w-14', className)}
+      size={size}
     >
       {isLoading ? <Loader2 className='w-6 h-4 animate-spin' /> : children}
     </Button>
--- src/pages/Visit/components/CustomerModal.tsx
+++ src/pages/Visit/components/CustomerModal.tsx
@@ -3,6 +3,10 @@ import { Separator } from '@/components/ui/separator';
 import { useCustomer } from '@/hooks/data/useCustomer';
 import { customFormatDate } from '@/utils/format-date.util';
 import VisitTab from './VisitTab/VisitTab';
+import { Plus } from 'lucide-react';
+import { useVisitMutation } from '@/hooks/data/useVisitMutation';
+import AsyncButton from '@/components/AsyncButton';
+import { useQueryClient } from '@tanstack/react-query';
 
 interface CustomerModalProps {
   open: boolean;
@@ -13,11 +17,29 @@ interface CustomerModalProps {
 const CustomerModal = ({ open, onOpenChange, customerId }: CustomerModalProps) => {
   const { customer, isLoading } = useCustomer(customerId);
 
+  const { createMutation: createVisitMutation } = useVisitMutation();
+
+  const queryClient = useQueryClient();
+
+  const handleCreateVisit = () => {
+    if (!customerId) return;
+    createVisitMutation.mutate(
+      {
+        customerId,
+      },
+      {
+        onSuccess: async () => {
+          await queryClient.invalidateQueries({
+            queryKey: ['visits', '1', '100', customerId],
+          });
+        },
+      }
+    );
+  };
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
-
-  // console.log('CustomerModal');
 
   return (
     <Dialog
@@ -26,9 +48,20 @@ const CustomerModal = ({ open, onOpenChange, customerId }: CustomerModalProps) =
     >
       <DialogContent className='max-w-full h-screen'>
         <DialogHeader>
-          <DialogTitle className='mb-2'>
-            {customer?.name} | {customer?.parentPhone} | {customer?.gender === 'MALE' ? 'Nam' : 'Nữ'} |{' '}
-            {customer?.birthDate ? customFormatDate(new Date(customer?.birthDate)) : ''}
+          <DialogTitle className='mb-2 flex justify-start items-center'>
+            <span>
+              {customer?.name} | {customer?.parentPhone} | {customer?.gender === 'MALE' ? 'Nam' : 'Nữ'} |{' '}
+              {customer?.birthDate ? customFormatDate(new Date(customer?.birthDate)) : ''} |
+            </span>
+            <AsyncButton
+              className='ml-2'
+              size='sm'
+              onClick={handleCreateVisit}
+              isLoading={createVisitMutation.isPending}
+            >
+              <Plus />
+              <span>Thêm lượt khám mới</span>
+            </AsyncButton>
           </DialogTitle>
           <Separator />
         </DialogHeader>
